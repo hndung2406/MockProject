dc24a64da41eb46ad563f256eece621b33f7824a
codercanloc <dinhhoang751989@gmail.com>
2018-10-11 13:40:10 +0700

    ghg

--- MOCK_PROJET_GROUP_ONE/src/main/java/com/fa/group01/action/CheckoutAction.java
+++
@@ -1,9 +0,0 @@
-package com.fa.group01.action;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class CheckoutAction extends ActionSupport {
-
-	private static final long serialVersionUID = 1L;
-
-}
