1b942a373b38d2497e78df470683d63b92236c9f
Nguyễn Thành Linh <thanhlinh12314@gmail.com>
2018-10-20 22:00:42 +0700

    part of test product dao

--- MOCK_PROJET_GROUP_ONE/src/test/java/com/fa/group01/dao/productdao/impl/ProductDAOImplTest.java
+++ MOCK_PROJET_GROUP_ONE/src/test/java/com/fa/group01/dao/productdao/impl/ProductDAOImplTest.java
@@ -1,48 +1,111 @@
 package com.fa.group01.dao.productdao.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import com.fa.group01.dao.productdao.ProductDAO;
+import com.fa.group01.connect.DatabaseConnect;
+import com.fa.group01.constants.DbQuery;
+import com.fa.group01.entity.Manufacture;
 import com.fa.group01.entity.Product;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductDAOImplTest {
 
-	private ProductDAO productDAO;
+	@Mock
+	private ProductDAOImpl productDao;
+
+	@Mock
+	private Connection mockConnection;
+	@Mock
+	private PreparedStatement mockPreparedStatement;
+	@Mock
+	private DatabaseConnect mockDatabaseConnect;
 
 	@Before
 	public void setUp() throws Exception {
-		productDAO = new ProductDAOImpl();
+
 	}
 
 	@Test
-	public void CanGetListProducts() throws SQLException {
-		List<Product> products = productDAO.findAllProduct();
-		products.forEach(System.out::println);
-		assertNotEquals(products.size(), 0);
-	}
-
-	@Test	
-	public void CanGetLimitNumberOfProduct() {
-		List<Product> products;
-		products = productDAO.fetchLimitNumberOfProducts(0, 3);
-		products.forEach(System.out::println);
-		assertNotEquals(products.size(), 0);
-
+	public void testAddProductSuccess() throws SQLException {
+		Manufacture manufacture = new Manufacture(1, "Apple");
+		Product product = new Product("P001", "Name", 123, "Description", "imageUrl", 3, "condition",new Date(2018, 11, 19), "spec", "properties", manufacture);
+		Mockito.when(productDao.addProduct(product)).thenReturn(1);
+		
+		assertEquals(productDao.addProduct(product), 1);
 	}
 	
 	@Test
-	public void CanGetProductsFromSpecificDate() {
-		List<Product> products = productDAO.fetchProductsFromSpecificDate("2018-10-01");
-		products.forEach(System.out::println);
-		assertNotEquals(products.size(),0);
+	public void testAddProductFail() throws SQLException {
+		Manufacture manufacture = new Manufacture(1, "Apple");
+		Product product = new Product("P001", "Name", 123, "Description", "imageUrl", 3, "condition",new Date(2018, 11, 19), "spec", "properties", manufacture);
+		Mockito.when(productDao.addProduct(product)).thenReturn(0);
+		
+		assertEquals(productDao.addProduct(product), 0);
 	}
+	
+	@Test
+	public void testFindAllProductNotNull() {
+		Mockito.when(productDao.findAllProduct()).thenReturn(new ArrayList<>());
+		assertNotNull(productDao.findAllProduct());
+	}
+	
+	@Test
+	public void testFindAllProductSize1() {
+		Manufacture manufacture = new Manufacture(1, "Apple");
+		Product product = new Product("P001", "Name", 123, "Description", "imageUrl", 3, "condition",new Date(2018, 11, 19), "spec", "properties", manufacture);
+		List<Product> products = new ArrayList<>();
+		products.add(product);
+		Mockito.when(productDao.findAllProduct()).thenReturn(products);
+		assertEquals(productDao.findAllProduct().size(), 1);
+	}
+	
+	@Test
+	public void testFindAllProductNull() {
+		Mockito.when(productDao.findAllProduct()).thenReturn(null);
+		assertNull(productDao.findAllProduct());
+	}
+	
+//	@Test
+//	public void test
+
+//	@Test
+//	public void CanGetListProducts() throws SQLException {
+//		List<Product> products = productDAO.findAllProduct();
+//		products.forEach(System.out::println);
+//		assertNotEquals(products.size(), 0);
+//	}
+//
+//	@Test	
+//	public void CanGetLimitNumberOfProduct() {
+//		List<Product> products;
+//		products = productDAO.fetchLimitNumberOfProducts(0, 3);
+//		products.forEach(System.out::println);
+//		assertNotEquals(products.size(), 0);
+//
+//	}
+//	
+//	@Test
+//	public void CanGetProductsFromSpecificDate() {
+//		List<Product> products = productDAO.fetchProductsFromSpecificDate("2018-10-01");
+//		products.forEach(System.out::println);
+//		assertNotEquals(products.size(),0);
+//	}
 
 }
